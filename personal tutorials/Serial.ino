d7a0d2bacc74765ac2c6702b12bd971ef2e6ba51
Kelson André Venâncio Teixeira <txkelson@gmail.com>
2018-11-01 18:36:51 -0300

    upload Seria.ino

---
+++ personal tutorials/Serial.ino
@@ -0,0 +1,28 @@
+ // How to use the serial
+
+
+ // in the setup function we must call the begin of the Serial
+
+void setup (){
+    Serial.begin (9600);
+}
+
+ //now you we should have something to shoow on Serial, so I will create a variable call x and print it
+
+void loop (){
+    int x = 0;
+   
+    //let's print it:
+
+    Serial.print (x);
+
+    // let's increment x
+   
+    x++;
+    
+    //this loop function goes very fast, so let slow it down
+
+    sleep (1000);
+
+    // It's done.
+}
